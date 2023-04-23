92c2e5f5bb358b781e3132272ec38ead0b399445
Yusif Ahmadov <77955404+yusifahmadov@users.noreply.github.com>
2023-04-23 22:29:10 +0400

    Solved

--- search_insert_position.dart
+++ search_insert_position.dart
@@ -19,6 +19,6 @@ class Solution {
 }
 
 void main(List<String> args) {
-  int result = Solution.searchInsert([1, 3, 5, 6], 2);
+  int result = Solution.searchInsert([1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12], 11);
   print(result);
 }
