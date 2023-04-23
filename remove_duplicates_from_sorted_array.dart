895f081d98f4f6149195632605ab2dc0039eefbf
Yusif Ahmadov <77955404+yusifahmadov@users.noreply.github.com>
2023-04-23 17:18:54 +0400

    "Remove duplicates from sorted array" is solved

--- remove_duplicates_from_sorted_array.dart
+++ remove_duplicates_from_sorted_array.dart
@@ -1,26 +1,14 @@
 class Solution {
   static int removeDuplicates(List<int> nums) {
-    List<String> expectedNums = nums.map((e) => e.toString()).toList();
-
-    // for (var i = 0; i < expectedNums.length; i++) {
-    //   for (var j = 1; j < expectedNums.length - 1; j++) {
-    //     if (expectedNums[i] == expectedNums[j] && expectedNums[j] != '_') {
-    //       expectedNums[j] = '_';
-    //     }
-    //   }
-    // }
-
-    for (var i = 0; i < expectedNums.length - 1; i++) {
-      for (var j = 1; j < expectedNums.length; j++) {
-        String curr = expectedNums[j];
-        if (expectedNums[i] == '_' && expectedNums[j] != '_') {
-          expectedNums[i] = curr;
-          expectedNums[j] = "_";
-        }
+    int l = 1;
+    for (var i = 1; i < nums.length; i++) {
+      if (nums[i] != nums[i - 1]) {
+        nums[l] = nums[i];
+        l++;
       }
     }
-    print(expectedNums);
-    return expectedNums.length;
+    print(nums);
+    return l;
   }
 }
 
