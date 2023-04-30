8925fc2e148033e36e66dd8ba6ac7c5eb52a7496
Yusif Ahmadov <77955404+yusifahmadov@users.noreply.github.com>
2023-04-30 13:32:27 +0400

    Solved

--- top_k_frequent_elements_347_medium.dart
+++ top_k_frequent_elements_347_medium.dart
@@ -1,39 +1,13 @@
 class Solution {
   static List<int> topKFrequent(List<int> nums, int k) {
-    /// Time complexity must be better than O(nlogn)
-    Map<int, int> map1 = {};
-
-    for (var i = 0; i < nums.length; i++) {
-      // n
-      if (!map1.containsKey(nums[i])) {
-        //n
-        map1.addAll({nums[i]: 1});
-      }
+    Map<int, int> freq = {};
+    for (int num in nums) {
+      freq[num] = (freq[num] ?? 0) + 1;
     }
+    List<int> uniqueNums = freq.keys.toList();
 
-    for (var i = 0; i < nums.length - 1; i++) {
-      //n
-      for (var j = i + 1; j < nums.length; j++) {
-        // n^2
-        if (nums[i] == nums[j]) {
-          map1.update(nums[i], (value) {
-            return value + 1;
-          });
-          nums.remove(nums[j]);
-          j = i + 1;
-        } else {}
-      }
-    }
-
-    List result = map1.entries.toList(); // n
-
-    result.sort(
-      (a, b) => b.value.compareTo(a.value),
-    ); // For small O(n^2), big lists -> O(n log(n))
-    print(result);
-    return result.map((e) => e.key as int).toList().getRange(0, 2).toList(); //n
-
-    /// Our func time complexity is n^2 so result will be "time limit exceeded"
+    uniqueNums.sort((a, b) => freq[b]!.compareTo(freq[a]!));
+    return uniqueNums.sublist(0, k);
   }
 }
 
